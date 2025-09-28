fdf5af22dbb0fb4988c781a1863f10f34a665188
Oscar Lozano <srempalador2021@gmail.com>
2025-09-28 03:05:09 -0500

    Creación del componente hijo MainEscritorio e importarlo al componente padre Main

--- src/components/main/escritorio/MainEscritorio.tsx
+++ src/components/main/escritorio/MainEscritorio.tsx
@@ -1,9 +1,9 @@
 import img from '../../../assets/Gemini_Generated_Image_aedt7waedt7waedt.png'
 
-const MainPortatiles = () => {
+const MainEscritorio = () => {
     return (
         <section className="w-full flex flex-col justify-between">
-            <h1 className="tex-lg font-bold md:text-2xl lg:text-3xl">Portatiles</h1>
+            <h1 className="tex-lg font-bold mt-5 md:text-2xl lg:text-3xl">Escritorio</h1>
             <hr className="w-28 border-gray-500 md:w-38" />
             <section className="w-full grid grid-cols-2 gap-4 mt-5 md:grid-cols-4 lg:grid-cols-6">
                 <article className="w-full border border-solid border-gray-500 cursor-pointer flex flex-col justify-center items-center transform transition-all duration-300 pb-2 rounded-lg shadow-2xl focus:-translate-y-2 hover:-translate-y-2 hover:scale-105">
@@ -13,7 +13,7 @@ const MainPortatiles = () => {
                     <div className='w-full px-2'>
                         <p className="text-lg font-bold md:text-xl">Acer</p>
                         <p>Especificaciones</p>
-                        <p><span className='font-bold md:text-lg'>$800.000</span></p>
+                        <p><span className='font-bold md:text-lg'>$1.800.000</span></p>
                     </div>
                 </article>
                 <article className="w-full border border-solid border-gray-500 cursor-pointer flex flex-col justify-center items-center transform transition-all duration-300 pb-2 rounded-lg shadow-2xl hover:-translate-y-2 hover:scale-105">
@@ -23,7 +23,7 @@ const MainPortatiles = () => {
                     <div className='w-full px-2'>
                         <p className="text-lg font-bold md:text-xl">Lenovo</p>
                         <p>Especificaciones</p>
-                        <p><span className='font-bold md:text-lg'>$800.000</span></p>
+                        <p><span className='font-bold md:text-lg'>$2.300.000</span></p>
                     </div>
                 </article>
                 <article className="w-full border border-solid border-gray-500 cursor-pointer flex flex-col justify-center items-center transform transition-all duration-300 pb-2 rounded-lg shadow-2xl hover:-translate-y-2 hover:scale-105">
@@ -33,7 +33,7 @@ const MainPortatiles = () => {
                     <div className='w-full px-2'>
                         <p className="text-lg font-bold md:text-xl">Hp</p>
                         <p>Especificaciones</p>
-                        <p><span className='font-bold md:text-lg'>$800.000</span></p>
+                        <p><span className='font-bold md:text-lg'>$8.000.000</span></p>
                     </div>
                 </article>
                 <article className="w-full border border-solid border-gray-500 cursor-pointer flex flex-col justify-center items-center transform transition-all duration-300 pb-2 rounded-lg shadow-2xl hover:-translate-y-2 hover:scale-105">
@@ -43,11 +43,11 @@ const MainPortatiles = () => {
                     <div className='w-full px-2'>
                         <p className="text-lg font-bold md:text-xl">Asus</p>
                         <p>Especificaciones</p>
-                        <p><span className='font-bold md:text-lg'>$800.000</span></p>
+                        <p><span className='font-bold md:text-lg'>$3.800.000</span></p>
                     </div>
                 </article>                
             </section>
         </section>
     );
 };
-export default MainPortatiles;
+export default MainEscritorio;
