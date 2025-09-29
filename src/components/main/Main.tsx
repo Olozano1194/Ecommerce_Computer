6ecfc241aff1fee91eea7ee08c58133933abbbc4
Oscar Lozano <srempalador2021@gmail.com>
2025-09-28 19:14:44 -0500

    Creación del componente hijo MainTablet e importarlo al componente padre Main

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,12 +1,14 @@
 import MainPortatiles from "./portatiles/MainPortatiles";
 import MainEscritorio from "./escritorio/MainEscritorio";
+import MainTablet from "./tablet/MainTablet";
 
 
 const Main = () => {
     return (
         <main className="w-full flex flex-col px-4 md:px-7">
             <MainPortatiles />
-            <MainEscritorio />            
+            <MainEscritorio />
+            <MainTablet />            
         </main>
         
     );
--- src/components/main/escritorio/MainEscritorio.tsx
+++ src/components/main/escritorio/MainEscritorio.tsx
@@ -5,13 +5,13 @@ const MainEscritorio = () => {
         <section className="w-full flex flex-col justify-between">
             <h1 className="tex-lg font-bold mt-5 md:text-2xl lg:text-3xl">Escritorio</h1>
             <hr className="w-28 border-gray-500 md:w-38" />
-            <section className="w-full grid grid-cols-2 gap-4 mt-5 md:grid-cols-4 lg:grid-cols-6">
+            <section className="w-full grid grid-cols-2 gap-4 mt-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
                 <article className="w-full border border-solid border-gray-500 cursor-pointer flex flex-col justify-center items-center transform transition-all duration-300 pb-2 rounded-lg shadow-2xl focus:-translate-y-2 hover:-translate-y-2 hover:scale-105">
                     <div className='w-full'>
                         <img src={img} alt="img-portatiles" className='w-full h-24 rounded-t-lg' />
                     </div>
                     <div className='w-full px-2'>
-                        <p className="text-lg font-bold md:text-xl">Acer</p>
+                        <p className="text-lg font-bold md:text-xl">Dell</p>
                         <p>Especificaciones</p>
                         <p><span className='font-bold md:text-lg'>$1.800.000</span></p>
                     </div>
@@ -21,7 +21,7 @@ const MainEscritorio = () => {
                         <img src={img} alt="img-portatiles" className='w-full h-24 rounded-t-lg' />
                     </div>
                     <div className='w-full px-2'>
-                        <p className="text-lg font-bold md:text-xl">Lenovo</p>
+                        <p className="text-lg font-bold md:text-xl">Toshiba</p>
                         <p>Especificaciones</p>
                         <p><span className='font-bold md:text-lg'>$2.300.000</span></p>
                     </div>
@@ -31,7 +31,7 @@ const MainEscritorio = () => {
                         <img src={img} alt="img-portatiles" className='w-full h-24 rounded-t-lg' />
                     </div>
                     <div className='w-full px-2'>
-                        <p className="text-lg font-bold md:text-xl">Hp</p>
+                        <p className="text-lg font-bold md:text-xl">Apple</p>
                         <p>Especificaciones</p>
                         <p><span className='font-bold md:text-lg'>$8.000.000</span></p>
                     </div>
